d4dcfdaec84e1dec2bc5231f6594347dfe215fbe
JayAshton <47352640+JayAshton@users.noreply.github.com>
2024-08-16 10:26:39 +0100

    Create config.fish

---
+++ config.fish
@@ -0,0 +1,11 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+# Fish Theme Stuff
+# See https://github.com/oh-my-fish/oh-my-fish/blob/master/docs/Themes.md#bobthefish
+set -g theme_powerline_fonts yes
+set -g theme_nerd_fonts no
+set -g theme_color_scheme dark
+set -g theme_display_git_master_branch yes
+set -g theme_git_worktree_support yes
