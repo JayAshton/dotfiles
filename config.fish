450af86e002c9e3b073e357edf443abaa7721945
JayAshton <47352640+JayAshton@users.noreply.github.com>
2024-11-20 10:59:15 +0000

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,15 +1,15 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
-end
-
 # Do not show any greeting
 function fish_greeting; end
-function fish_right_prompt; end
 
-# Fish Theme Stuff
-# See https://github.com/oh-my-fish/oh-my-fish/blob/master/docs/Themes.md#bobthefish
-set -g theme_powerline_fonts yes
-set -g theme_nerd_fonts yes
-set -g theme_color_scheme terminal-dark
-set -g theme_display_git_master_branch yes
-set -g theme_git_worktree_support yes
+# bobthefish theme
+set -g theme_display_git yes
+set -g theme_display_git_dirty yes
+set -g theme_display_git_untracked no
+set -g theme_display_git_ahead_verbose no
+set -g theme_display_git_dirty_verbose no
+set -g theme_display_git_master_branch no
+set -g theme_git_worktree_support no
+set -g theme_powerline_fonts no
+set -g theme_color_scheme dark
+set -g theme_display_user ssh
+set -g theme_display_date no
