acb87b8a491280d3f9a1a2afc9be5b5461eeb1d3
JayAshton <47352640+JayAshton@users.noreply.github.com>
2024-11-20 11:22:23 +0000

    Update config.fish

--- config.fish
+++ config.fish
@@ -9,7 +9,8 @@ set -g theme_display_git_ahead_verbose no
 set -g theme_display_git_dirty_verbose no
 set -g theme_display_git_master_branch no
 set -g theme_git_worktree_support no
-set -g theme_powerline_fonts no
+set -g theme_powerline_fonts yes
+set -g theme_nerd_fonts no
 set -g theme_color_scheme dark
 set -g theme_display_user ssh
 set -g theme_display_date no
