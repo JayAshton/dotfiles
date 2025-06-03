8467292828aab3ac35596a0e42f93cf147fa6d33
JayAshton <47352640+JayAshton@users.noreply.github.com>
2025-06-03 09:07:39 +0100

    Update config.fish

--- config.fish
+++ config.fish
@@ -1,16 +1,28 @@
-# Do not show any greeting
+#ilancosman/tide@v6
+#jorgebucaran/nvm.fish
+#edc/bass
+#reitzig/sdkman-for-fish@v2.0.0
+
 function fish_greeting; end
 
-# bobthefish theme
-set -g theme_display_git yes
-set -g theme_display_git_dirty yes
-set -g theme_display_git_untracked no
-set -g theme_display_git_ahead_verbose no
-set -g theme_display_git_dirty_verbose no
-set -g theme_display_git_master_branch no
-set -g theme_git_worktree_support no
-set -g theme_powerline_fonts yes
-set -g theme_nerd_fonts no
-set -g theme_color_scheme dark
-set -g theme_display_user ssh
-set -g theme_display_date no
+set -gx PATH /Users/jay.ashton/.nvm/versions/node/v22.12.0/bin $PATH
+set -gx PATH /Users/jay.ashton/.sdkman/candidates/java/current/bin $PATH
+set -gx PATH /Users/jay.ashton/.docker/bin $PATH
+set -gx PATH /opt/homebrew/bin/docker-compose $PATH
+set -gx PATH /opt/homebrew/bin $PATH
+
+set -gx DOCKER_HOST unix://$HOME/.colima/default/docker.sock
+set -gx EDITOR "code --wait"
+
+alias ls="ls -a --color=auto"
+alias yarninstall="npm install --global yarn"
+alias yarncode="yarn dlx @yarnpkg/sdks vscode"
+
+bass source "$HOME/.sdkman/bin/sdkman-init.sh"
+
+bass source /opt/homebrew/opt/nvm/nvm.sh
+bass source /opt/homebrew/opt/nvm/etc/bash_completion.d/nvm
+
+if not set -q TMUX; and test "$TERM" = "xterm-kitty"
+    tmux attach || exec tmux new-session
+end
